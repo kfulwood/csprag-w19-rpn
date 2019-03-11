3882a002a92e68420cf65cd512051a18048bf60c
Kiya Fulwood <kfulwood@umich.edu>
2019-03-10 20:24:11 -0400

    Add carat test for rpn
    This tests the '^' implementation of the rpn

--- test_rpn.py
+++ test_rpn.py
@@ -5,4 +5,7 @@ class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate("1 1 +")
         self.assertEqual(2, result)
-
+    
+    def test_carat(self):
+        result = rpn.calculate("3 2 ^")
+        self.assertEqual(9, result)
