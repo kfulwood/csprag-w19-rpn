07704daf40cfe04934c8bc4f42a5e1cdb2f00868
Kiya Fulwood <kfulwood@umich.edu>
2019-03-10 20:17:54 -0400

    Implement rpn Calc

--- rpn.py
+++ rpn.py
@@ -1,7 +1,31 @@
 #!/usr/bin/env python3
 
+import operator
+
+
+operators = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+        '/': operator.truediv,
+}
+
 def calculate(arg):
-    pass
+    stack = list()
+    for token in myarg.split():
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
 
 def main():
     while True:
